540426ad541973bb198fe39d4d6726dd471ba871
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-06-08 19:22:47 +0430

    1401-03-18 Added  BillOwnershipOrganization to project

---
+++ src/PunishmentOrg.Api.Core/Service/Anu/PunishmentOrg/BillOwnershipOrganizationServicesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PunishmentOrg.Api.Core.ServiceBase.Anu.PunishmentOrg;
+
+namespace PunishmentOrg.Api.Core.Service.Anu.PunishmentOrg;
+
+[ApiController]
+[Route("[controller]")]
+public class BillOwnershipOrganizationServicesController : ControllerBase, IBillOwnershipOrganizationServicesBase
+{
+    [HttpGet(Name = "SendBillOwnership")]
+    public void SendBillOwnership()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet(Name = "ReceiveBillOwnership")]
+    public void ReceiveBillOwnership()
+    {
+        throw new NotImplementedException();
+    }
+}
