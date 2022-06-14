ac1d6dcfbb82c0d3b1b2ca67328d37d10070d7fa
pouya Rezaieh <p.rezaieh@P-REZAIEH>
2022-06-14 16:34:10 +0430

    1401-03-23-Add First PCase Repository Method

---
+++ src/PunishmentOrg.DataAccess/Repositories/PunishmentOrg/Anu/PunishmentOrg/PCase/PCaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using Anu.PunishmentOrg.Case;
+using Anu.PunishmentOrg.DiscoveryMinutes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PunishmentOrg.DataAccess.Repositories.PunishmentOrg
+{
+    public class PCaseRepository : GenericRepository<PCase>, Domain.Interface.PunishmentOrg.IPCaseRepository
+    {
+        public PCaseRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<PCase> GetTerminateCaseByNo(string no)
+        {
+            return await _context.Set<PCase>().FirstOrDefaultAsync(x => x.No == no);
+        }
+    }
+}
