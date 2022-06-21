9b1de68e8c089d369bf15033da5252ef1e728f55
d.moosazadeh <d.moosazadeh@D-MOOSAZADEH>
2022-06-21 17:42:39 +0430

    1401-03-31-AddAbstractValidation

---
+++ Validations/Validations.PunishmentOrg/Validations.PunishmentOrg/Validation/IAbstractValidation.cs
@@ -0,0 +1,9 @@
+﻿namespace Validations.PunishmentOrg.Validation
+{
+    public interface IAbstractValidation<TEntity> where TEntity : class
+    {
+        abstract List<System.ComponentModel.DataAnnotations.ValidationResult> Execute(TEntity entity);
+
+        bool NeedExecute();
+    }
+}
