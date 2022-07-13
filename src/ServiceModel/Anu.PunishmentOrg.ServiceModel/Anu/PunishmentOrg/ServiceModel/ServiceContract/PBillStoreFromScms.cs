0cb1472fba368ce84473a253311fb07a92c52067
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-07-13 19:47:14 +0430

    1401-04-22 Added product list to the entity

--- src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Anu/PunishmentOrg/ServiceModel/ServiceContract/PBillStoreFromScms.cs
+++ src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Anu/PunishmentOrg/ServiceModel/ServiceContract/PBillStoreFromScms.cs
@@ -7,47 +7,53 @@ namespace Anu.PunishmentOrg.ServiceModel.ServiceContract
     {
         public PBillStoreFromScms()
         {
-            this.ThePBillStorePersonScmsList = new List<PBillStorePersonScms>(); 
+            this.ThePBillStorePersonScmsList = new List<PBillStorePersonScms>();
+            this.ThePBillStoreProductList=new List<PBillStoreProduct>();
         }
-            
-            [System.ComponentModel.DisplayName("تاريخ قبض")]
-            public string BillDate { get; set ;} 
-            
-            
-            [System.ComponentModel.DisplayName("شماره قبض انبار")]
-            public string BillNumber { get; set ;} 
-            
-            
-            [System.ComponentModel.DisplayName("کدينگ دستگاه کاشف")]
-            public string CodingDeviceDetector { get; set ;} 
-            
-            
-            [System.ComponentModel.DisplayName("شناسه يکتاي پرونده")]
-            public string ParentContentNumber { get; set ;} 
-            
-            
-            [System.ComponentModel.DataAnnotations.Required]
-            [System.ComponentModel.DisplayName("کلمه عبور")]
-            public string PassWord { get; set ;} 
-            
-            
-            [System.ComponentModel.DisplayName("شماره صورتجلسه")]
-            public string ProceedingNumber { get; set ;} 
-            
-            
-            [System.ComponentModel.DataAnnotations.Required]
-            [System.ComponentModel.DisplayName("اشخاص قبض انبار")]
-            public List<PBillStorePersonScms> ThePBillStorePersonScmsList { get; set ;} 
-            
-            
-            [System.ComponentModel.DisplayName("شناسه سامانه جامع انبارها")]
-            public string TrackingCodeStores { get; set ;} 
-            
-            
-            [System.ComponentModel.DataAnnotations.Required]
-            [System.ComponentModel.DisplayName("نام کاربري")]
-            public string UserName { get; set ;} 
-            
+
+        [System.ComponentModel.DisplayName("تاريخ قبض")]
+        public string BillDate { get; set; }
+
+
+        [System.ComponentModel.DisplayName("شماره قبض انبار")]
+        public string BillNumber { get; set; }
+
+
+        [System.ComponentModel.DisplayName("کدينگ دستگاه کاشف")]
+        public string CodingDeviceDetector { get; set; }
+
+
+        [System.ComponentModel.DisplayName("شناسه يکتاي پرونده")]
+        public string ParentContentNumber { get; set; }
+
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("کلمه عبور")]
+        public string PassWord { get; set; }
+
+
+        [System.ComponentModel.DisplayName("شماره صورتجلسه")]
+        public string ProceedingNumber { get; set; }
+
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("اشخاص قبض انبار")]
+        public List<PBillStorePersonScms> ThePBillStorePersonScmsList { get; set; }
+        
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("خروجي کالا/ارز قبض/رسيد کالا/ارز صادر شده")]
+        public List<PBillStoreProduct> ThePBillStoreProductList { get; set; }
+
+
+        [System.ComponentModel.DisplayName("شناسه سامانه جامع انبارها")]
+        public string TrackingCodeStores { get; set; }
+
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("نام کاربري")]
+        public string UserName { get; set; }
+
 
     }
-    }
+}
