e21b77f19f19e8fcec137edddecbac01ad25d3bc
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-07-13 19:58:32 +0430

    1401-04-22 Added Scms method

---
+++ src/PunishmentOrg.Api.Core/Anu/PunishmentOrg/ServiceBase/IPBillStoreBase.cs
@@ -0,0 +1,24 @@
+﻿using Anu.PunishmentOrg.ServiceModel.ServiceRequest;
+using Anu.PunishmentOrg.ServiceModel.ServiceResponse;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anu.PunishmentOrg.ServiceBase
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class IPBillStoreBase : ControllerBase
+    {
+        protected readonly Anu.DataAccess.IUnitOfWork _unitOfWork;
+
+
+        public IPBillStoreBase(Anu.DataAccess.IUnitOfWork unitOfWork)
+        {
+
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("ReceivePBillStoreFromScms")]
+        public abstract Task<ReceivePBillStoreFromScmsResponse> ReceivePBillStoreFromScms([FromBody] ReceivePBillStoreFromScmsRequest request);
+
+    }
+}
