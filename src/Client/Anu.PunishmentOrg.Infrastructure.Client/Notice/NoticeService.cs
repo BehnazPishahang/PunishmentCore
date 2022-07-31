a6bec7a333be9217771863e8f97085a44668bcc6
Ali NematiShishegaran <a.nemati@VM1-G9>
2022-07-31 08:03:10 -0700

    CALL NOTICE SERVICE DONE

---
+++ src/Client/Anu.PunishmentOrg.Infrastructure.Client/Notice/NoticeService.cs
@@ -0,0 +1,37 @@
+﻿using Anu.PunishmentOrg.ServiceModel.Notice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace Anu.PunishmentOrg.Client.Infrastructure.Notice
+{
+    public  static class NoticeService
+    {
+
+      static string Baseurl = "http://192.168.1.14:94/";
+
+      static string ServiceAdderss = "api/v2/BillStore/InqueryPNoticeList";
+        public static IEnumerable<PNoticeContract> getPNoticeList(string nationalCode)
+        {
+
+            PNoticeInqueryRequest inputData = new PNoticeInqueryRequest();
+            inputData.NationalityCode = (long)Convert.ToDouble(nationalCode);
+
+           
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(Baseurl);
+          
+            var response =  client.PostAsJsonAsync(ServiceAdderss, inputData).Result;
+
+            PNoticeInqueryResponse result = response.Content.ReadAsAsync<PNoticeInqueryResponse>().Result;
+           
+            return result.PNoticeList.AsEnumerable();
+
+        }
+    }
+}
