1c4b5c2db2dd11ff2a50dc386e1671273a0fbc04
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-08-01 20:20:10 +0430

    1401-05-10 Added generated files and implemented the code

--- src/ServiceModel/Anu.PunishmentOrg.ServiceModel/BillStore/PBillStoreServiceResponse.cs
+++ src/ServiceModel/Anu.PunishmentOrg.ServiceModel/BillStore/PBillStoreServiceResponse.cs
@@ -1,18 +1,22 @@
 ﻿
+using Anu.Commons.ServiceModel.ServiceResponse;
+using Anu.PunishmentOrg.ServiceModel.ServiceContract;
+
 namespace Anu.PunishmentOrg.ServiceModel.BillStore
 {
     [System.ComponentModel.DisplayName("خروجی سرویس دریافت قبض انبار")]
-    public class PBillStoreServiceResponse
+    public class PBillStoreServiceResponse : PBillStoreResponse ,IResponseMessage
     {
+
+        public Result Result { get; set; }
+
         public PBillStoreServiceResponse()
         {
+            Result = new Result();
         }
             
-                [System.ComponentModel.DataAnnotations.Required]
             [System.ComponentModel.DisplayName("قبض/رسيد کالا/ارز صادر شده توسط سازمان مسئول نگه داري کالا/ارز در سرویس")]
         public Anu.PunishmentOrg.ServiceModel.BillStore.PBillStoreContract? ThePBillStoreContract { get; set; }
-
-            
-
+       
     }
     }
