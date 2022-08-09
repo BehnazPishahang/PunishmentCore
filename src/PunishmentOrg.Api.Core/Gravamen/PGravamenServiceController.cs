f898f9ac029d510ce117940aefc6e2c4b3b5644c
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-08-09 14:50:56 +0430

    1401-05-18 Added service models and service

---
+++ src/PunishmentOrg.Api.Core/Gravamen/PGravamenServiceController.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Anu.PunishmentOrg.Api.Gravamen
+{
+    public class PGravamenServiceController : PGravamenServiceControllerBase
+    {
+        #region Constructor
+        public PGravamenServiceController()
+        {
+        }
+        #endregion Constructor
+
+        #region Properties
+        #endregion Properties
+
+        #region Overrides
+        #endregion Overrides
+
+        #region Methods
+        #endregion Methods
+    }
+}
---
+++ src/PunishmentOrg.Api.Core/Gravamen/PGravamenServiceControllerBase.cs
@@ -0,0 +1,17 @@
+﻿
+using Anu.PunishmentOrg.ServiceModel.Gravamen;
+using Anu.PunishmentOrg.ServiceModel.Gravamen;
+
+namespace Anu.PunishmentOrg.Api.Gravamen
+{
+    public abstract class PGravamenServiceControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+    public PGravamenServiceControllerBase()
+    {
+    }
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+            [Microsoft.AspNetCore.Mvc.Route("api/v1/[controller]/[action]")]
+        public abstract PGravamenServiceResponse RecieveGravamen([Microsoft.AspNetCore.Mvc.FromBody]PGravamenServiceRequest request);
+
+    }
+    }
