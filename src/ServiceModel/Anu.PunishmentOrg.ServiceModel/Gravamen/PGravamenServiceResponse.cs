4976d8659ea88145c6188fe59fc7cd82f906bf9d
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-08-10 13:51:59 +0430

    1401-05-19 Added necessary codes

--- src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Gravamen/PGravamenServiceResponse.cs
+++ src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Gravamen/PGravamenServiceResponse.cs
@@ -1,12 +1,17 @@
 ﻿
+using Anu.Commons.ServiceModel.ServiceResponse;
+
 namespace Anu.PunishmentOrg.ServiceModel.Gravamen
 {
     [System.ComponentModel.DisplayName("خروجی سرویس شکوائیه")]
-    public class PGravamenServiceResponse
+    public class PGravamenServiceResponse : IResponseMessage
     {
+        public Result Result { get; set; }
+
         public PGravamenServiceResponse()
         {
+            Result = new Result();
         }
 
     }
-    }
+}
