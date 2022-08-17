09607e863be38e2627b9f05db103678c5481d4fa
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-08-17 17:16:25 +0430

    1401-05-26 Added repositories to use generic repository :/🤦‍♂️🤦‍♂️🤦‍♂️

---
+++ src/DataAccess/Anu.PunishmentOrg.DataAccess/PGravamen/PGravamenRepository.cs
@@ -0,0 +1,14 @@
+﻿using Anu.DataAccess.Repositories;
+using Anu.PunishmentOrg.Domain.PGravamen;
+
+namespace Anu.PunishmentOrg.DataAccess.PGravamen;
+
+public class PGravamenRepository : GenericRepository<DataModel.Gravamen.PGravamen>, IPGravamenRepository
+{
+    public PGravamenRepository(Anu.DataAccess.ApplicationDbContext context) : base(context)
+    {
+    }
+
+}
+
+
