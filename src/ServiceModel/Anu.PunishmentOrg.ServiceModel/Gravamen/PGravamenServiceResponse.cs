31e91ee8c2152428a1bfd70a90afdb38a8956b1e
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-08-17 17:17:09 +0430

    1401-05-26 Added FollowupNumber to response

--- src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Gravamen/PGravamenServiceResponse.cs
+++ src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Gravamen/PGravamenServiceResponse.cs
@@ -1,10 +1,11 @@
 ﻿
 using Anu.Commons.ServiceModel.ServiceResponse;
+using Anu.PunishmentOrg.ServiceModel.ServiceContract;
 
 namespace Anu.PunishmentOrg.ServiceModel.Gravamen
 {
     [System.ComponentModel.DisplayName("خروجی سرویس شکوائیه")]
-    public class PGravamenServiceResponse : IResponseMessage
+    public class PGravamenServiceResponse : PGravamenResponse, IResponseMessage
     {
         public Result Result { get; set; }
         public PGravamenServiceResponse()
