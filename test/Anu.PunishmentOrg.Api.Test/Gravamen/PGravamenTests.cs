e136102d8ad0d7d8232c70b283efb05e0bd3de05
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-08-24 14:30:14 +0430

    1401-06-02 Added Some necessary tests

--- test/Anu.PunishmentOrg.Api.Test/Gravamen/PGravamenTests.cs
+++ test/Anu.PunishmentOrg.Api.Test/Gravamen/PGravamenTests.cs
@@ -10,14 +10,15 @@ using Anu.Commons.ServiceModel.ServiceResponseEnumerations;
 using Anu.PunishmentOrg.ServiceModel.ServiceResponseEnumerations;
 using Anu.PunishmentOrg.Enumerations;
 using System;
+using System.Text;
 
 namespace Anu.PunishmentOrg.Api.Test.Gravamen;
 
-//! کل من علیها fun
 public class PGravamenTests
 {
     private readonly Mock<Anu.DataAccess.IUnitOfWork> _unitOfWork = new();
     private readonly PGravamenServiceController controller;
+    private readonly PGravamen pGravamen;
 
 
     /// xUnit uses the constructor for set up method
@@ -27,9 +28,99 @@ public class PGravamenTests
         //        .ReturnsAsync((IEnumerable<PGravamen>)null);
 
         controller = new PGravamenServiceController(_unitOfWork.Object);
+
+        #region PGravamen Instatiation
+        var p1 = new PGravamenPerson()
+        {
+
+            Name = "",
+            Family = "",
+            Address = "",
+            BirthDate = "",
+            FatherName = "",
+            IdentityNumber = "",
+            MobilNumber = "",
+            NationalCode = "",
+            Nationality = BaseInfo.Enumerations.LNationality.Iranian,
+            PersonStartPost = Enumerations.PUPersonStartPost.PlaintiffPerson,
+            Sex = BaseInfo.Enumerations.SexType.Male,
+            PersonType = BaseInfo.Enumerations.PersonType.NaturalPerson,
+            PostCode = "",
+            TradeUnitName = "",
+            PersonPassword = "",
+        };
+        var p2 = new PGravamenPerson()
+        {
+
+            Name = "",
+            Family = "",
+            Address = "",
+            BirthDate = "",
+            FatherName = "",
+            IdentityNumber = "",
+            MobilNumber = "",
+            NationalCode = "",
+            Nationality = BaseInfo.Enumerations.LNationality.Iranian,
+            PersonStartPost = Enumerations.PUPersonStartPost.OffendingPerson,
+            Sex = BaseInfo.Enumerations.SexType.Male,
+            PersonType = BaseInfo.Enumerations.PersonType.NaturalPerson,
+            PostCode = "",
+            TradeUnitName = "KH",
+            PersonPassword = "",
+        };
+
+        var v = new PGravamenViolation()
+        {
+
+            RowNumber = 1,
+            SubjectTitle = "a",
+            ViolationAddress = "b",
+            ViolationDate = "c",
+            ViolationDesc = "d",
+            ViolationPrice = 123,
+        };
+
+        var a = new PGravamenAttachment()
+        {
+            FileExtension = BaseInfo.Enumerations.FileExtension.PDF,
+            TheAttachmentType = new BaseInfo.DataModel.Types.AttachmentType()
+            {
+                Code = "300",
+                Id = "300",
+                Title = "پيوست شكوائيه مردمي",
+                Timestamp = 1
+            },
+            TheGAttachmentData = new Anu.BaseInfo.DataModel.Attachment.GAttachmentData()
+            {
+                DocFile = Encoding.ASCII.GetBytes("QW51IGNvcG9yYXRpb24=")
+            }
+        };
+
+        pGravamen = new PGravamen()
+        {
+
+            PetitionSubject = "d",
+            PetitionDescription = "e",
+            NoticeText = "f",
+            PetitionReasons = "g",
+            RejectReasonText = "h",
+            ReporterName = "i",
+            ReporterFamily = "j",
+            ReporterMobilNumber = "k",
+
+            ThePGravamenPersonList = new List<PGravamenPerson>() { p1, p2 },
+            ThePGravamenAttachmentList = new List<PGravamenAttachment>() { a },
+            ThePGravamenViolationList = new List<PGravamenViolation>() { v },
+
+            CreateDateTime = "l",
+            FollowUpNo = "m",
+            HowDataType = Enumerations.PU135OrWebSite.WebSite,
+            GravamenOrReport = Anu.PunishmentOrg.Enumerations.GravamenOrReport.Gravamen,
+        };
+        #endregion
     }
 
-    [Fact]
+    [Fact(Skip = "This scenario will be passed after all of the other scenarios have passed.", DisplayName = "Success Scenario")]
     public void RecieveGravamen_SuccessfullyExecuted_ShouldReturnSuccessfulResult()
     {
         //Arrange
@@ -68,7 +159,6 @@ public class PGravamenTests
         //Assert
         Assert.Equal((int)AnuResult.Successful, result.Result.Result.Code);
     }
-
 
     [Fact]
     public void RecieveGravamen_RequestIsNull_ShouldReturnRequestIsNullOrCorrupt()
@@ -122,7 +212,7 @@ public class PGravamenTests
         //Assert
         Assert.Equal((int)PGravamenResult.PGravamen_Field_IsNullOrInvalid, result.Result.Result.Code);
     }
-     [Fact]
+    [Fact]
     public void RecieveGravamen_OneOfNecessaryPositionsIsMissing_ShouldReturnPGravamenNecessaryPositionsAreNotAvailabe()
     {
         //Arrange
@@ -162,6 +252,11 @@ public class PGravamenTests
         Assert.Equal((int)PGravamenResult.PGravamen_NecessaryPositions_AreNotAvailabe, result.Result.Result.Code);
     }
 
+    [Fact]
+    public void RecieveGravamen_OneOfPlaintiffPersonNecessaryFiledsIsMissing_ReturnsPGravamenPlatiffNecessaryFieldIsNullOrInvalid()
+    {
+       
+    }
 
 
     ///xUnit uses the Dispose for tear down method
