2b59c013b146f0ac8edd607bdfbae093743724d5
d.moosazadeh <d.moosazadeh@D-MOOSAZADEH>
2022-08-25 12:29:26 +0430

    1401-06-04-FixGenericRepository

--- src/Domain/Anu.Domain/IGenericRepository.cs
+++ src/Domain/Anu.Domain/IGenericRepository.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Anu.Domain
 {
-    public interface IGenericRepository<T> where T : class
+    public interface IGenericRepository<T> : IGenericRepository
+        where T : class
     {
         Task<T> GetById(string id);
         Task<IEnumerable<T>> GetAll();
@@ -17,5 +18,9 @@ namespace Anu.Domain
         Task AddRange(IEnumerable<T> entities);
         Task Remove(T entity);
         Task RemoveRange(IEnumerable<T> entities);
+    }
+
+    public interface IGenericRepository
+    {
     }
 }
