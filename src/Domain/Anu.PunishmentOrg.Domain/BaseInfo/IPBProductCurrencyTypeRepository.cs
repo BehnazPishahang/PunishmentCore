1eb96a808ba94bd6b7de34c6b466566c75a93aa9
nona kord <n.kord@NONAKORD>
2022-09-07 18:19:12 +0430

    1401-06-16-Add BaseInfoServices

---
+++ src/Domain/Anu.PunishmentOrg.Domain/BaseInfo/IPBProductCurrencyTypeRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Anu.PunishmentOrg.Domain.BaseInfo
+{
+    public interface IPBProductCurrencyTypeRepository : Anu.Domain.IGenericRepository<DataModel.BaseInfo.PBProductCurrencyType>
+    {
+        public Task<DataModel.BaseInfo.PBProductCurrencyType> GetByCode(string code);
+        public Task<string> GetMaxLevelCode(string levelcode);
+    }
+}
