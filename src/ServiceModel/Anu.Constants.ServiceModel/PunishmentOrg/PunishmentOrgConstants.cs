9ef55a1453358b9e38e0044e13d1bf45d2bbc3cf
s.kavianmehr <s.kavianmehr@S-KAVIANI>
2022-09-10 20:03:42 +0430

    compelete

--- src/ServiceModel/Anu.Constants.ServiceModel/PunishmentOrg/PunishmentOrgConstants.cs
+++ src/ServiceModel/Anu.Constants.ServiceModel/PunishmentOrg/PunishmentOrgConstants.cs
@@ -60,6 +60,11 @@ namespace Anu.Constants.ServiceModel.PunishmentOrg
             /// فراخواننده سرويس دريافت درخواست مجوز تعقيب/بازرسي و پاسخ آن
             /// </summary>
             public const string SendPChaseLicenseReqList = "025";
+
+            /// <summary>
+            /// کاربر سامانه 135 تعزیرات
+            /// </summary>
+            public const string Tazirat135Users = "026";
         }
 
         public static class NajaUnit
@@ -128,6 +133,12 @@ namespace Anu.Constants.ServiceModel.PunishmentOrg
             /// ستاد مركزي سازمان غذا و دارو
             /// </summary>
             public const string FoodDrugOrganization = "280000";
+
+            /// <summary>
+            /// سازمان تعزیرات حکومتی
+            /// این ناجا یونیت برای کاربران سامانه ی 135 تعزیرات استفاده شده است
+            /// </summary>
+            public const string PunishmentOrg = "00006";
         }
 
         public enum NajaUnitCode
