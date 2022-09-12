acd2f3fb662c864ee84bcee4f2197a4443cab760
Ali NematiShishegaran <a.nemati@VM1-G9>
2022-09-12 04:29:36 -0700

    1401-06-21 refactoring done

---
+++ src/Client/BlazorComponents/MudBlazor.ThemeManager/Enums/ColorTitles.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazor.ThemeManager.Enums;
+
+public enum ColorTitles
+{
+    [Description("اصلی ")] Primary,
+    [Description("ثانویه")] Secondary,
+    [Description("سومین")] Tertiary,
+    [Description("اعلان")] Info,
+    [Description("موفق")] Success,
+    [Description("هشدار")] Warning,
+    [Description("ایراد")] Error,
+    [Description("تاریک")] Dark,
+   
+}
---
+++ src/Client/BlazorComponents/MudBlazor.ThemeManager/Models/ThemeManagerTheme/ThemeManagerTheme.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor.ThemeManager.Enums;
+
+namespace MudBlazor.ThemeManager.Models.ThemeManagerTheme;
+
+internal class ThemeManagerTheme
+{
+    public string PresetThemes { get; set; }
+    public Modes Mode { get; set; }
+    public ThemeManagerThemePalette Palette { get; set; } = new();
+
+    public Typography FontSetting { get; set; }
+    public int DefaultBorderRadiusAsInt { get; set; }
+
+    public int DefaultFontSizeAsInt { get; set; } 
+    public LayoutProperties LayoutProperties { get; set; }
+}
