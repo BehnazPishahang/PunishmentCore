80670e6d61956b998aedddbdf1f7fec67de66d6e
pouyarzh <100574895+pouyarzh@users.noreply.github.com>
2022-09-12 21:06:03 +0430

    1401-06-21

--- src/PunishmentOrg.Api.Core/Gravamen/PGravamenServiceController.cs
+++ src/PunishmentOrg.Api.Core/Gravamen/PGravamenServiceController.cs
@@ -141,41 +141,46 @@ namespace Anu.PunishmentOrg.Api.Gravamen
             int docFilesLength = 0;
             foreach (var attachment in request.ThePGravamenContract!.TheGAttachmentContractList!)
             {
-                var docFile = attachment.TheGAttachmentDataContract!.DocFile;
+                //todo: should be fix this bug in client and then in the backend should be refactore this section
+                if (!attachment.TheGAttachmentDataContract.Null())
+                {
+                    if (!attachment!.TheGAttachmentDataContract!.DocFile.Null())
+                    {
+                        var docFile = attachment.TheGAttachmentDataContract!.DocFile;
+                        docFile.NullOrEmpty(PGravamenResult.PGravamen_NoFileIsAttached);
 
                         docFilesLength += docFile!.Length;
 
-                var validateDocFilesSize = ValidateDocFilesSize(docFilesLength);
-                if (!validateDocFilesSize.Null())
-                {
-                    return validateDocFilesSize;
-                }
-                
+                        var validateDocFilesSize = ValidateDocFilesSize(docFilesLength);
+                        if (!validateDocFilesSize.Null())
+                        {
+                            return validateDocFilesSize;
+                        }
 
 
                         var attachmentType = await _unitOfWork.Repositorey<IGenericRepository<AttachmentType>>().GetById(Anu.Constants.ServiceModel.BaseInfo.BaseInfoConstants.AttachmentTypeId.GravamenAttachmentTypeId);
 
 
 
-                var attachedFile = new PGravamenAttachment()
-                {
-                    Id = Guid.NewGuid().ToString("N"),
-                    Timestamp = 1,
-                    FileExtension = attachment.FileExtension,
-                    SaveAttachmentType = Anu.BaseInfo.Enumerations.SaveAttachmentType.SaveInDataBase,
-                    CreateDateTime = DateTime.Now.ToPersian().ToString(),
-                    TheAttachmentType = attachmentType,
+                        var attachedFile = new PGravamenAttachment()
+                        {
+                            Id = Guid.NewGuid().ToString("N"),
+                            Timestamp = 1,
+                            FileExtension = attachment.FileExtension,
+                            SaveAttachmentType = Anu.BaseInfo.Enumerations.SaveAttachmentType.SaveInDataBase,
+                            CreateDateTime = DateTime.Now.ToPersian().ToString(),
+                            TheAttachmentType = attachmentType,
+                            Title = attachment.Title,
+                            TheGAttachmentData = new GAttachmentData()
+                            {
+                                Id = Guid.NewGuid().ToString("N"),
+                                Timestamp = 1,
+                                DocFile = attachment.TheGAttachmentDataContract!.DocFile
 
-                    TheGAttachmentData = new GAttachmentData()
-                    {
-                        Id = Guid.NewGuid().ToString("N"),
-                        Timestamp = 1,
-                        DocFile = attachment.TheGAttachmentDataContract!.DocFile
+                            }
+                        };
+                        attachedFile.TheAttachmentType = attachmentType;
 
-                    }
-                };
-                attachedFile.TheAttachmentType = attachmentType;
-                
 
                         attachmentList.Add(attachedFile);
                     }
@@ -197,8 +202,8 @@ namespace Anu.PunishmentOrg.Api.Gravamen
                 ReporterFamily      = string.Empty,
                 ReporterMobilNumber = string.Empty,
 
-                ThePGravamenPersonList     = personList,
-                ThePGravamenAttachmentList = attachmentList,
+                ThePGravamenPersonList = personList,
+                ThePGravamenAttachmentList = attachmentList.Count == 0 ? null : attachmentList,
 
                 CreateDateTime   = DateTime.Now.ToPersian().ToString(),
                 FollowUpNo       = followupNumber,
