52a9e3e6c6333ee1799940b158a16f0f565e7116
pouyarzh <100574895+pouyarzh@users.noreply.github.com>
2022-09-14 16:33:19 +0430

    1401-06-23 add GAttachmentData Repository

---
+++ src/Domain/Anu.BaseInfo.Domain/GAttachment/IGAttachmentDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anu.BaseInfo.Domain.GAttachment;
+
+public interface IGAttachmentDataRepository : Anu.Domain.IGenericRepository<Anu.BaseInfo.DataModel.Attachment.GAttachmentData>
+{
+    public Task InsertIntoGAttachmentData(byte[] docfile);
+
+}
