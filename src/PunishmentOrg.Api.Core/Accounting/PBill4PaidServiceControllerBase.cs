72bf7497e35f6c8b1999c3fc615d95040f168503
Majid Abbasi <m.abbasi201@gmail.com>
2022-09-15 13:24:49 +0330

    1401-06-24-FixBog

--- src/PunishmentOrg.Api.Core/Accounting/PBill4PaidServiceControllerBase.cs
+++ src/PunishmentOrg.Api.Core/Accounting/PBill4PaidServiceControllerBase.cs
@@ -10,7 +10,7 @@ namespace Anu.PunishmentOrg.Api.Accounting
     {
     }
         [Microsoft.AspNetCore.Mvc.HttpPost]
-            [Microsoft.AspNetCore.Mvc.Route("api/v1/PBill4Paid/GetPBill4PaidListByFishNo")]
+            [Microsoft.AspNetCore.Mvc.Route("api/v1/PBill4Paid/GetPBill4PaidByFishNo")]
         public abstract Task<GetPBill4PaidByFishNoResponse> GetPBill4PaidByFishNo([Microsoft.AspNetCore.Mvc.FromBody]GetPBill4PaidByFishNoRequest request);
 
         [Microsoft.AspNetCore.Mvc.HttpPost]
