398232062a69c136e42705ac4fa7be8432acf219
Majid Abbasi <m.abbasi201@gmail.com>
2022-09-18 09:20:03 +0330

    1401-06-27-EditServiceModels

--- src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Accounting/GetPBill4PaidByFishNoResponse.cs
+++ src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Accounting/GetPBill4PaidByFishNoResponse.cs
@@ -6,10 +6,11 @@ using Anu.PunishmentOrg.ServiceModel.Notice;
 namespace Anu.PunishmentOrg.ServiceModel.Accounting
 {
     [System.ComponentModel.DisplayName("GetPBill4PaidByFishNo")]
-    public class GetPBill4PaidByFishNoResponse
+    public class GetPBill4PaidByFishNoResponse : IResponseMessage
     {
         public GetPBill4PaidByFishNoResponse()
         {
+            Result = new Result();
             ThePBill4PaidInfoContract = new PBill4PaidInfoContract();
         }
 
@@ -17,7 +18,7 @@ namespace Anu.PunishmentOrg.ServiceModel.Accounting
             [System.ComponentModel.DisplayName("وضعیت پرداخت در سرویس")]
         public PBill4PaidInfoContract? ThePBill4PaidInfoContract { get; set; }
 
-            
+        public Result Result { get; set; }
 
     }
     }
