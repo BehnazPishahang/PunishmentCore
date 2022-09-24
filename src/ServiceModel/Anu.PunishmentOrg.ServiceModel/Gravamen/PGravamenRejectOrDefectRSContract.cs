2801066bd5e2f33d99cd7bdce6902c6891d56222
Majid Abbasi <m.abbasi201@gmail.com>
2022-09-24 12:40:10 +0330

    1401-07-02-CompleteGravamenContractInGetByIdOutPut

---
+++ src/ServiceModel/Anu.PunishmentOrg.ServiceModel/Gravamen/PGravamenRejectOrDefectRSContract.cs
@@ -0,0 +1,18 @@
+﻿
+using Anu.PunishmentOrg.ServiceModel.ServiceContract;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Anu.PunishmentOrg.ServiceModel.Gravamen
+{
+    [System.ComponentModel.DisplayName("دلایل رد / نقص شکوائیه در سرویس")]
+    public class PGravamenRejectOrDefectRSContract
+    {
+        public PGravamenRejectOrDefectRSContract()
+        {
+            ThePBGravamenRejectDefectType = new PBGravamenRejectDefectTypeContract();
+        }
+
+        [System.ComponentModel.DisplayName("دلایل رد")]
+        public PBGravamenRejectDefectTypeContract ThePBGravamenRejectDefectType { get; set; }
+    }
+}
