42eba702b3e17af8f8496bd121b1a09fd2af9b87
mohammadreza shafie <m.shafie@D-MOUSAVI>
2022-09-26 15:11:03 +0330

    1401-07-04 Dependency injection is implemented in the test project

---
+++ test/Anu.PunishmentOrg.Api.Test/Helpers/TestHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anu.PunishmentOrg.Api.Test.Helpers
+{
+    internal static class TestHelper
+    {
+        internal static IConfigurationRoot GetIConfigurationRoot(string outputPath)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(outputPath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
