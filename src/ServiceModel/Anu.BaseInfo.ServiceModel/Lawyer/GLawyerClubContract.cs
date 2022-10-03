7182a2279d19133df279fba15c729a291294d651
Majid Abbasi <m.abbasi201@gmail.com>
2022-10-03 12:09:45 +0330

    1401-07-11-HotFix

---
+++ src/ServiceModel/Anu.BaseInfo.ServiceModel/Lawyer/GLawyerClubContract.cs
@@ -0,0 +1,29 @@
+﻿namespace Anu.BaseInfo.ServiceModel.Lawyer
+{
+    [System.ComponentModel.DisplayName("کانون هاي وکلاي کشور در سرویس")]
+    public class GLawyerClubContract
+    {
+        public GLawyerClubContract()
+        {
+        }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("كد")]
+        public string? Code { get; set; }
+
+
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("وضعيت")]
+        public Enumerations.State? State { get; set; }
+
+
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DisplayName("عنوان")]
+        public string? Title { get; set; }
+
+
+
+    }
+}
